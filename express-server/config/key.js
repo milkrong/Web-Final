19d17040b366c08a310a3305327ff466dd10ccad
milkrong <milkrong121@outlook.com>
2018-12-10 19:09:34 -0500

    Create key.js

---
+++ express-server/config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://music-manager:music-manager1@ds145113.mlab.com:45113/postify-music',
+  secretKey: 'postify',
+  osskey: 'Leo2nIRrZjgFHBIma9EVm5mmVXwBFy',
+};
